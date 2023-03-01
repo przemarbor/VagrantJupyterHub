34f2492e24c1593365444384eb8074b2ce32e837
przemarbor <60915641+przemarbor@users.noreply.github.com>
2023-03-01 01:04:40 +0100

    Update initializeVagrantMachine.sh

--- initializeVagrantMachine.sh
+++ initializeVagrantMachine.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
-# # # only once
+### PREREQUISITIES
 sudo cp /etc/apt/sources.list /etc/apt/sources.listBKP
 
 sudo tee -a /etc/apt/sources.list<<EOF
 deb http://deb.debian.org/debian bullseye main contrib non-free
 deb-src http://deb.debian.org/debian bullseye main contrib non-free
-deb http://security.debian.org/debian-security bullseye-security main
-deb-src http://security.debian.org/debian-security bullseye-security main
+
+deb http://security.debian.org/debian-security bullseye-security main contrib
+deb-src http://security.debian.org/debian-security bullseye-security main contrib
+
 deb http://deb.debian.org/debian bullseye-updates main contrib non-free
 deb-src http://deb.debian.org/debian bullseye-updates main contrib non-free
+
 deb http://deb.debian.org/debian bullseye-backports main contrib non-free
 deb-src http://deb.debian.org/debian bullseye-backports main contrib non-free
 EOF
